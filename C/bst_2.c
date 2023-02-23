0e2ad58e2823979674c4067f4a8c11c052644c9a
Nikhilkb2255 <115878497+Nikhilkb2255@users.noreply.github.com>
2023-02-23 21:53:47 +0530

    Update bst_2.c

--- C/bst_2.c
+++ C/bst_2.c
@@ -1,147 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node{
-    int data;
-    struct node *l,*r;
-}*root=NULL,*newnode;
-
-void insert(int n){
-    newnode=(struct node*)malloc(sizeof(struct node));
-    struct node *r,*p;
-    newnode->data=n;
-    newnode->l=newnode->r=NULL;
-    if(root==NULL)root=newnode;
-    else{
-        r=root;
-        while(r!=NULL){
-            if(n<r->data){
-                p=r;
-                r=r->l;
-            }
-            else{
-                p=r;
-                r=r->r;
-            }
-        }
-        if(n<p->data)
-            p->l=newnode;
-        else
-            p->r=newnode;
+ 
+int n, i, j, u, v, f=1;
+int cost[10][10], visited[10]={0}, mincost=0, min;
+ 
+void main()
+{
+    printf("\nEnter the number of nodes:");
+    scanf("%d",&n);
+ 
+	printf("\nEnter the adjacency matrix:\n");
+    for(i=1;i<=n;i++)
+    for(j=1;j<=n;j++)
+    {
+        scanf("%d",&cost[i][j]);
+        if(cost[i][j]==0)
+            cost[i][j]=999;
     }
-
-}
-
-struct node* delete(struct node *p,int n){
-    struct node *t;
-    if(p==NULL)return p;
-    if(n<p->data){
-        p->l=delete(p->l,n);
-    }
-    else if(n>p->data){
-        p->r=delete(p->r,n);
-    }
-    else{
-        if(p->l==NULL){
-            t=p->r;
-            free(p);
-            return t;
+ 
+	visited[1]=1;
+    printf("\n");
+ 
+	while(f < n)
+    {
+        for(i=1,min=999;i<=n;i++)
+        for(j=1;j<=n;j++)
+ 
+		if(cost[i][j]< min)
+        if(visited[i]!=0)
+        {
+            min=cost[i][j];
+            u=i;
+            v=j;
         }
-        else if(p->r==NULL){
-            t=p->l;
-            free(p);
-            return t;
+ 
+		if(visited[u]==0 || visited[v]==0)
+        {
+            printf("\n Edge %d:(%d %d) cost:%d",f++,u,v,min);
+            mincost+=min;
+            visited[v]=1;
         }
-        
-            t=p->r;
-            while(t && t->l != NULL ){
-                t=t->l;
-            }
-            p->data=t->data;
-            p->r=delete(p->r,t->data);
-        }
-        return p;
-    }
-    
-
-struct node* search(int n){
-    struct node *r=root,*p;
-    if(root->data==n){
-        return root;
-    }
-    while(r!=NULL){
-        if(n<r->data){
-                p=r;
-                r=r->l;
-        }
-        else if(n>r->data){
-            p=r;
-            r=r->r;
-        }
-        else{
-            return r;
-        }
-    }
-    return NULL;
-    
-
-}
-
-
-void inorder(struct node *r){
-    if(r==NULL)return;
-    inorder(r->l);
-    printf("%d\t",r->data);
-    inorder(r->r); 
-
-}
-
-
-void preorder(struct node *r){
-    if(r==NULL)return;
-    printf("%d\t",r->data);
-    preorder(r->l);
-    preorder(r->r);
-    
-}
-
-
-void postorder(struct node *r){
-    if(r==NULL)return;
-    postorder(r->l);
-    postorder(r->r);
-    printf("%d\t",r->data);
-}
-
-
-void main(){
-    int c,n;
-    struct node *s;
-    while(1){
-    printf("\n\nMENU\n----\n\n\t1.Insert\n\t2.Delete\n\t3.Display\n\t4.Exit\n\n\t\tEnter choice:");
-        scanf("%d",&c);
-        switch(c){
-            case 1:printf("\n\tEnter number to be inserted:");
-                    scanf("%d",&n);
-                    insert(n);
-                    break;
-            case 2:printf("\n\tEnter number to be deleted:");
-                    scanf("%d",&n);
-                    if(search(n))
-                        delete(root,n);
-                    else{
-                        printf("\ndata not found!deletion cannot perform");
-                    }
-                    break;
-            case 3:printf("\nElements:");
-                    inorder(root);
-                    printf("\nElements (preorder):");
-                    preorder(root);
-                    printf("\nElements (postorder):");
-                    postorder(root);
-                    break;
-            case 4:exit(0);
-                    break;
-            default:printf("Invalid");
-        }
-    }
+ 
+		cost[u][v]=cost[v][u]=999;
+ 
+	}
+ 
+	printf("\n Minimun cost=%d",mincost);
+ 
 }
